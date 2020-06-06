a3913ba13d6897006acfc2bc4a1e5ac6f00a5209
arbaaj19 <60877294+arbaaj19@users.noreply.github.com>
2020-06-06 12:42:45 +0530

    first

--- financial_portfolia/src/App.js
+++ financial_portfolia/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import MyStock from './componant/mystock';
-import AddStock from'./componant/addstocks';
-function App() {
-  return (
-    <div>
-    <MyStock></MyStock>
-    <AddStock></AddStock>  
-    </div>  
-  );
+import Navbar from './components/Navbar';
+import Table from './components/Table';
+import Stocks from './components/Stocks';
+
+class App extends Component {
+
+   render() {
+
+      return (
+         <div className='App'>
+            <Navbar />
+            <Table />
+            <Stocks />
+         </div>
+      )
+   }
 }
 
 export default App;
